f61c96c8adc7b30bdd1e2858fa331997372b03e2
Lali Gabelaya <laligabelaya@gmail.com>
2019-03-09 16:08:00 -0800

    will paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_post, only: [:show]
   def index
-    @posts = Post.all.limit(10).includes(:photos, :user, :likes).order('created_at desc')
+    @post =  Post.paginate(:page => params[:page], :per_page => 5).includes(:photos, :user, :likes).order("created_at desc")
     @post = Post.new
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+before_action :authenticate_user!
 def index
   @users = User.search(params[:term])
   respond_to :js
